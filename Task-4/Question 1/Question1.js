e6815d49675821382d51d174acbdd8f1a1c817b5
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-07-11 14:56:53 +0530

    updated

--- Task-4/Question 1/Question1.js
+++ Task-4/Question 1/Question1.js
@@ -1,23 +1,23 @@
 // 1) a.Finding OddNumbers USing Anonymous functions
 
-const Oddno = function(array){
-    for(let i in array){
-          if(array[i]%2!=0){
-             console.log(array[i])
-          } 
-     }
-  }
-
-  console.log(Oddno([1,2,3,4,5,6,7,]))
-
-
-  // 1) a.Finding OddNumbers USing IIFE functions
-
-let total = (function(arr){
-    for(let ind in arr){
-        if(arr[ind]%2!=0){
-           console.log(arr[ind])
-        } 
+const Oddno = function (array) {
+   for (let i in array) {
+      if (array[i] % 2 != 0) {
+         console.log(array[i])
+      }
    }
-})([1,2,3,4,5,6,67])
+}
+
+console.log(Oddno([1, 2, 3, 4, 5, 6, 7,]))
+
+
+// 1) a.Finding OddNumbers USing IIFE functions
+
+let total = (function (arr) {
+   for (let ind in arr) {
+      if (arr[ind] % 2 != 0) {
+         console.log(arr[ind])
+      }
+   }
+})([1, 2, 3, 4, 5, 6, 67])
 console.log(total)
